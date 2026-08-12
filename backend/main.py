1d095538be8f68239dfc1db18554b12ce899b25a
Edythaf <edytha.feo22@gmail.com>
2026-08-12 10:24:29 +0700

    Complete KelanaAI console app

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,34 @@
 #Variables store the trip data
-destination = input("Desitnation : ")
-days = input("Days : ")
-budget = input("Budget : ")
-travel_style = input("Travel Style : ")
-hotel_cost = input("Hotel Cost :")
-transportation_cost = input("Transportation Cost : ")
-food_cost = input("Food Cost : ")
-miscellaneous_cost = input("Miscellaneous Cost: ")
-total_estimated_cost = (hotel_cost + transportation_cost + food_cost + miscellaneous_cost)
+destination = str(input("Desitnation : "))
+country = str(input("Country : "))
+days = int(input("Days : "))
+budget = float(input("Budget : "))
+currecy = str(input("Currency : "))
+travel_month = str(input("Travel month : "))
+
+
+#travel_style = input("Travel Style : ")
+#hotel_cost = input("Hotel Cost :")
+#transportation_cost = input("Transportation Cost : ")
+#food_cost = input("Food Cost : ")
+#miscellaneous_cost = input("Miscellaneous Cost: ")
+#total_estimated_cost = (hotel_cost + transportation_cost + food_cost + miscellaneous_cost)
 
 
 
-def print_trip_summary(destination, days, budget, travel_style):
+def print_trip_summary(destination, country, days, budget, currecy, travel_month):
     print("=======================")
     print("KelanaAI")
     print("=======================")
     print(f"Desitination    : {destination}")
+    print(f"Country         : {country}")
     print(f"Days            : {days} ")
     print(f"Budget          : {budget}")
-    print(f"Travel Style   : {travel_style}")
+    print(f"Currency        : {currecy}")
+    print(f"Travel Month    : {travel_month}")
+   # print(f"Travel Style   : {travel_style}")
 
-print_trip_summary(destination, days, budget, travel_style)
-if total_estimated_cost > budget:
-    print("⚠ Budget exceeded.")
+print_trip_summary(destination, country, days, budget, currecy, travel_month)
+#if total_estimated_cost > budget:
+ #   print("⚠ Budget exceeded.")
 
